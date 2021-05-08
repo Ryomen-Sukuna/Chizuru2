936ce3c0166aa749b3391db2888c402d25cc9332
Divu <70699947+imdivu@users.noreply.github.com>
2021-05-08 18:35:49 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -160,7 +160,7 @@ def start(update: Update, context: CallbackContext):
             if args[0].lower() == "help":
                 send_help(update.effective_chat.id, (gs(chat.id, "pm_help_text")))
             elif args[0].lower() in ["markdownhelp", "markdown"]:
-                IMPORTED["extras"].markdown_help_sender(update)
+                IMPORTED["mics"].markdown_help(update)
             elif args[0].lower() == "nations":
                 IMPORTED["nations"].send_nations(update)
             elif args[0].lower().startswith("stngs_"):
@@ -221,7 +221,7 @@ def error_callback(update: Update, context: CallbackContext):
         pass
         # handle all other telegram related errors
 
-@kigcallback(pattern=r'help_')
+@kigcallback(pattern=r'help_.*')
 def help_button(update: Update, context: CallbackContext):
     '''#TODO
 
@@ -375,12 +375,11 @@ def about_callback(update: Update, context: CallbackContext):
             text=gs(chat.id, "antispam_help"),
             parse_mode=ParseMode.MARKDOWN,
             reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton(text="Next", callback_data="aboutmanu_spamprot2")],
                 [InlineKeyboardButton(text="NPL", callback_data="aboutmanu_spamprotcf")],
                 [InlineKeyboardButton(text="Back", callback_data="aboutmanu_howto")]
             ]), 
         )
-        query.answer("Antispam 1")
+        query.answer("Antispam")
 
     elif query.data == "aboutmanu_spamprotcf":
         query.message.edit_text(
@@ -391,50 +390,6 @@ def about_callback(update: Update, context: CallbackContext):
             ]), 
         )
         query.answer("Chatroom Spam Prediction")
-
-    elif query.data == "aboutmanu_spamprot2":
-        query.message.edit_text(
-            text="*Anti-Spam Settings:*"
-                 "\n\n- /setflood <int/'no'/'off'>: enables or disables flood control"
-                 "\n- /setfloodmode <ban/kick/mute/tban/tmute> <value>: Action to perform when user have exceeded flood limit. ban/kick/mute/tmute/tban"
-                 "\n_Antiflood allows you to take action on users that send more than x messages in a row. Exceeding the set flood will result in restricting that user._"
-                 "\n\n- /addblacklist <triggers>: Add a trigger to the blacklist. Each line is considered one trigger, so using different lines will allow you to add multiple triggers."
-                 "\n- /blacklistmode <off/del/warn/ban/kick/mute/tban/tmute>: Action to perform when someone sends blacklisted words."
-                 "\n_Blacklists are used to stop certain triggers from being said in a group. Any time the trigger is mentioned, the message will immediately be deleted. A good combo is sometimes to pair this up with warn filters!_"
-                 "\n\n- /reports <on/off>: Change report setting, or view current status."
-                 "\n • If done in pm, toggles your status."
-                 "\n • If in chat, toggles that chat's status."
-                 "\n_If someone in your group thinks someone needs reporting, they now have an easy way to call all admins._",
-            parse_mode=ParseMode.MARKDOWN,
-            reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton(text="Previous", callback_data="aboutmanu_spamprot"),
-                InlineKeyboardButton(text="Next", callback_data="aboutmanu_spamprot3")],
-                [InlineKeyboardButton(text="NPL", callback_data="aboutmanu_spamprotcf")],
-                [InlineKeyboardButton(text="Back", callback_data="aboutmanu_howto")]
-            ]), 
-        )
-        query.answer("Antispam 2")
-
-    elif query.data == "aboutmanu_spamprot3":
-        query.message.edit_text(
-            text="*Anti-Spam Settings:*"
-                 "\n\n- /lock <type>: Lock items of a certain type (not available in private)"
-                 "\n- /locktypes: Lists all possible locktypes"
-                 "\n_The locks module allows you to lock away some common items in the telegram world; the bot will automatically delete them!_"
-                 "\n\n- /addwarn <keyword> <reply message>: Sets a warning filter on a certain keyword. If you want your keyword to be a sentence, encompass it with quotes, as such: /addwarn \"very angry\" This is an angry user. "
-                 "\n- /warn <userhandle>: Warns a user. After 3 warns, the user will be banned from the group. Can also be used as a reply."
-                 "\n- /strongwarn <on/yes/off/no>: If set to on, exceeding the warn limit will result in a ban. Else, will just kick."
-                 "\n_If you're looking for a way to automatically warn users when they say certain things, use the /addwarn command._"
-                 "\n\n- /welcomemute <off/soft/strong>: All users that join, get muted"
-                 "\n_ A button gets added to the welcome message for them to unmute themselves. This proves they aren't a bot! soft - restricts users ability to post media for 24 hours. strong - mutes on join until they prove they're not bots._",
-            parse_mode=ParseMode.MARKDOWN,
-            reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton(text="Previous", callback_data="aboutmanu_spamprot2")],
-                [InlineKeyboardButton(text="NPL", callback_data="aboutmanu_spamprotcf")],
-                [InlineKeyboardButton(text="Back", callback_data="aboutmanu_howto")]
-            ]), 
-        )
-        query.answer("Antispam 3")
 
     elif query.data == "aboutmanu_tac":
         query.message.edit_text(
