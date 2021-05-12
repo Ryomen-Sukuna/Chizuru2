b4f8b2c925dcd590a676b68a6ced07be3677d744
Divu <70699947+imdivu@users.noreply.github.com>
2021-05-12 11:21:09 +0530

    Update fun.py

--- tg_bot/modules/fun.py
+++ tg_bot/modules/fun.py
@@ -64,7 +64,7 @@ def slap(update: Update, context: CallbackContext):
     user_id = extract_user(message, args)
 
     if user_id == bot.id:
-        temp = random.choice(random.shuffle(fun_strings.SLAP_Kigyō_TEMPLATES))
+        temp = random.choice(random.shuffle(fun_strings.SLAP_BOT_TEMPLATES))
 
         if isinstance(temp, list):
             if temp[2] == "tmute":
