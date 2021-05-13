6ca49aa8543a146219497deb4a0c74ffc9bb5d22
Divu <70699947+imdivu@users.noreply.github.com>
2021-05-13 16:48:53 +0530

    Update misc.py

--- tg_bot/modules/misc.py
+++ tg_bot/modules/misc.py
@@ -28,29 +28,14 @@ from tg_bot.modules.helper_funcs.decorators import kigcmd
 import tg_bot.modules.helper_funcs.health as hp
 from tg_bot.modules.helper_funcs.get_time import get_time as get_readable_time
 
-MARKDOWN_HELP = f"""
-Markdown is a very powerful formatting tool supported by telegram. {dispatcher.bot.first_name} has some enhancements, to make sure that \
-saved messages are correctly parsed, and to allow you to create buttons.
+@kigcmd(command='stat', filters=Filters.chat_type.group)
+def stat(update: Update, _):
+    update.effective_message.reply_text(
+       f"<b>Total Message:</b> <code>{update.effective_message.message_id}</code>",
+       parse_mode=ParseMode.HTML,
+       timeout=60,
+    )
 
-- <code>_italic_</code>: wrapping text with '_' will produce italic text
-- <code>*bold*</code>: wrapping text with '*' will produce bold text
-- <code>`code`</code>: wrapping text with '`' will produce monospaced text, also known as 'code'
-- <code>[sometext](someURL)</code>: this will create a link - the message will just show <code>sometext</code>, \
-and tapping on it will open the page at <code>someURL</code>.
-EG: <code>[test](example.com)</code>
-
-- <code>[buttontext](buttonurl:someURL)</code>: this is a special enhancement to allow users to have telegram \
-buttons in their markdown. <code>buttontext</code> will be what is displayed on the button, and <code>someurl</code> \
-will be the url which is opened.
-EG: <code>[This is a button](buttonurl:example.com)</code>
-
-If you want multiple buttons on the same line, use :same, as such:
-<code>[one](buttonurl://example.com)
-[two](buttonurl://google.com:same)</code>
-This will create two buttons on a single line, instead of one button per line.
-
-Keep in mind that your message <b>MUST</b> contain some text other than just a button!
-"""
 
 @kigcmd(command='gifid')
 def gifid(update: Update, _):
