105f2edc326af9761e850e62e8bc0512d32eb7ac
Divu <70699947+imdivu@users.noreply.github.com>
2021-05-13 15:54:49 +0530

    Create health.py

---
+++ tg_bot/modules/helper_funcs/health.py
@@ -0,0 +1,83 @@
+
+from tg_bot.modules.sql.antispam_sql import is_user_gbanned
+from tg_bot.modules.sql.afk_sql import is_afk, check_afk_status
+from tg_bot.modules.sql.userinfo_sql import get_user_me_info, get_user_bio
+
+#HELTH-BAR - Show User Health -- This Feature From @SaitamaRobot
+def no_by_per(totalhp, percentage):
+    """
+    rtype: num of `percentage` from total
+    eg: 1000, 10 -> 10% of 1000 (100)
+    """
+    return totalhp * percentage / 100
+
+
+def get_percentage(totalhp, earnedhp):
+    """
+    rtype: percentage of `totalhp` num
+    eg: (1000, 100) will return 10%
+    """
+
+    matched_less = totalhp - earnedhp
+    per_of_totalhp = 100 - matched_less * 100.0 / totalhp
+    per_of_totalhp = str(int(per_of_totalhp))
+    return per_of_totalhp
+
+
+def hpmanager(user):
+    total_hp = (get_user_num_chats(user.id) + 10) * 10
+
+    if not is_user_gbanned(user.id):
+
+        # Assign new var `new_hp` since we need `total_hp` in
+        # end to calculate percentage.
+        new_hp = total_hp
+
+        # if no username decrease 25% of hp.
+        if not user.username:
+            new_hp -= no_by_per(total_hp, 25)
+
+        # if no lastname decrease 5% of hp.
+        if not user.last_name:
+            new_hp -= no_by_per(total_hp, 5)
+        try:
+            dispatcher.bot.get_user_profile_photos(user.id).photos[0][-1]
+        except IndexError:
+            # no profile photo ==> -25% of hp
+            new_hp -= no_by_per(total_hp, 25)
+        # if no /setme exist ==> -20% of hp
+        if not get_user_me_info(user.id):
+            new_hp -= no_by_per(total_hp, 20)
+        # if no bio exsit ==> -10% of hp
+        if not get_user_bio(user.id):
+            new_hp -= no_by_per(total_hp, 10)
+
+        if is_afk(user.id):
+            afkr = check_afk_status(user.id)
+            # if user is afk and no reason then decrease 7%
+            # else if reason exist decrease 5%
+            if not afkr.reason:
+                new_hp -= no_by_per(total_hp, 7)
+            else:
+                new_hp -= no_by_per(total_hp, 5)
+
+    # Bad status effects:
+    # gbanned users will always have 5% HP from max HP
+    # Example: If HP is 100 but gbanned
+    # Available HP is 5% of 100 = 5HP
+
+    else:
+        new_hp = no_by_per(total_hp, 5)
+
+    return {
+        "earnedhp": int(new_hp),
+        "totalhp": int(total_hp),
+        "percentage": get_percentage(total_hp, new_hp)
+    }
+
+
+def make_bar(per):
+    done = min(round(per / 10), 10)
+    return "▬" * done + "▭" * (10 - done)
+
+
