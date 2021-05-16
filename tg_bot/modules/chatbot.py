c64cd148c20d4b2f94e1a0053f4b4b8344c3e48e
Divu <70699947+imdivu@users.noreply.github.com>
2021-05-16 20:21:58 +0530

    Chatbot: Removed Random cmd

--- tg_bot/modules/chatbot.py
+++ tg_bot/modules/chatbot.py
@@ -31,7 +31,7 @@ def chatmode(update: Update, context: CallbackContext):
 
     if args[1].lower() in ("yes", "on"):
         if not is_chat:
-            sql.add_chat(chat.id, False)
+            sql.add_chat(chat.id)
             message.reply_text("Chatbot successfully enabled for this group!")
             logger = (f"<b>{html.escape(chat.title)}:</b>\n"
                       f"#AI_ENABLED\n"
@@ -50,17 +50,6 @@ def chatmode(update: Update, context: CallbackContext):
             return logger
         else:
             message.reply_text("Chatbot is already disabled for this group!")
-            return ""
-    elif args[1].lower() in ("random"):
-        if not is_chat:
-            sql.add_chat(chat.id, True)
-            message.reply_text("Chatbot successfully enabled for this group!")
-            logger = (f"<b>{html.escape(chat.title)}:</b>\n"
-                      f"#AI_ENABLED\n"
-                      f"<b>Admin:</b> {mention_html(user.id, user.first_name)}\n")
-            return logger
-        else:
-            message.reply_text("Chatbot is already enabled for this group!")
             return ""
     else:
          message.reply_text("Chatbot Status For This Group: <i>{}</i>".format("Enabled" if is_chat else "Disabled"))
