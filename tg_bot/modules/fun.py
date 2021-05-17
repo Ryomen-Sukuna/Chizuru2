7a1ccb91fd142215908fc75d7264a4734e8f77b3
Divu <70699947+imdivu@users.noreply.github.com>
2021-05-17 17:19:13 +0530

    Update fun.py

--- tg_bot/modules/fun.py
+++ tg_bot/modules/fun.py
@@ -5,19 +5,34 @@ import time
 import urllib.request
 import urllib.parse
 import requests
+
 from telegram import ParseMode, Update, ChatPermissions
 from telegram.ext import CallbackContext
 from telegram.error import BadRequest
 
-import tg_bot.modules.fun_strings as fun_strings
-from tg_bot import dispatcher
 from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.extraction import extract_user
 from tg_bot.modules.helper_funcs.decorators import kigcmd
+import tg_bot.modules.fun_strings as fun
+
+
+# truth / dare
+@kigcmd(command="truth")
+def dare(update, context):
+     xyz = requests.get("https://elianaapi.herokuapp.com/games/truth").json()
+     truth = xyz.get("truth")
+     update.effective_message.reply_text(truth)
+
+@kigcmd(command="dare")
+def dare(update, context):
+     xyz = requests.get("https://elianaapi.herokuapp.com/games/dares").json()
+     dare = xyz.get("dare")
+     update.effective_message.reply_text(dare)
+
 
 @kigcmd(command='runs')
-def runs(update: Update, context: CallbackContext):
-    update.effective_message.reply_text(random.choice(fun_strings.RUN_STRINGS))
+def runs(update, context):
+    update.effective_message.reply_text(random.choice(fun.RUN_STRINGS))
 
 @kigcmd(command='slap')
 def slap(update: Update, context: CallbackContext):
@@ -35,7 +50,7 @@ def slap(update: Update, context: CallbackContext):
     user_id = extract_user(message, args)
 
     if user_id == bot.id:
-        temp = random.choice(fun_strings.SLAP_Kigyō_TEMPLATES)
+        temp = random.choice(fun.SLAP_BOT_TEMPLATES)
 
         if isinstance(temp, list):
             if temp[2] == "tmute":
@@ -65,10 +80,10 @@ def slap(update: Update, context: CallbackContext):
         user1 = bot.first_name
         user2 = curr_user
 
-    temp = random.choice(fun_strings.SLAP_TEMPLATES)
-    item = random.choice(fun_strings.ITEMS)
-    hit = random.choice(fun_strings.HIT)
-    throw = random.choice(fun_strings.THROW)
+    temp = random.choice(fun.SLAP_TEMPLATES)
+    item = random.choice(fun.ITEMS)
+    hit = random.choice(fun.HIT)
+    throw = random.choice(fun.THROW)
     reply = temp.format(user1=user1, user2=user2, item=item, hits=hit, throws=throw)
 
     reply_text(reply, parse_mode=ParseMode.HTML)
@@ -113,33 +128,9 @@ def pat(update: Update, context: CallbackContext):
             reply_to_message_id=msg_id,
         )
 
-@kigcmd(command='roll')
-def roll(update: Update, context: CallbackContext):
-    update.message.reply_text(random.choice(range(1, 7)))
-
 @kigcmd(command='toss')
-def toss(update: Update, context: CallbackContext):
-    update.message.reply_text(random.choice(fun_strings.TOSS))
-
-@kigcmd(command='shrug')
-def shrug(update: Update, context: CallbackContext):
-    msg = update.effective_message
-    reply_text = (
-        msg.reply_to_message.reply_text if msg.reply_to_message else msg.reply_text
-    )
-    reply_text(r"¯\_(ツ)_/¯")
-
-@kigcmd(command='rlg')
-def rlg(update: Update, context: CallbackContext):
-    eyes = random.choice(fun_strings.EYES)
-    mouth = random.choice(fun_strings.MOUTHS)
-    ears = random.choice(fun_strings.EARS)
-
-    if len(eyes) == 2:
-        repl = ears[0] + eyes[0] + mouth[0] + eyes[1] + ears[1]
-    else:
-        repl = ears[0] + eyes[0] + mouth[0] + eyes[0] + ears[1]
-    update.message.reply_text(repl)
+def toss(update, context):
+    update.message.reply_text(random.choice(fun.TOSS))
 
 @kigcmd(command='decide')
 def decide(update: Update, context: CallbackContext):
@@ -148,20 +139,11 @@ def decide(update: Update, context: CallbackContext):
         if update.effective_message.reply_to_message
         else update.effective_message.reply_text
     )
-    reply_text(random.choice(fun_strings.DECIDE))
+    reply_text(random.choice(fun.DECIDE))
 
-@kigcmd(command='table')
-def table(update: Update, context: CallbackContext):
-    reply_text = (
-        update.effective_message.reply_to_message.reply_text
-        if update.effective_message.reply_to_message
-        else update.effective_message.reply_text
-    )
-    reply_text(random.choice(fun_strings.TABLE))
-
-from tg_bot.modules.language import gs
 
 def get_help(chat):
+    from tg_bot.modules.language import gs
     return gs(chat, "fun_help")
 
 __mod_name__ = "Fun"
