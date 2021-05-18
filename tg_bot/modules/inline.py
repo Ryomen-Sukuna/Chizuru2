4a639e094ebb36351c664a7fa67b172f9c5b32df
Divu <70699947+imdivu@users.noreply.github.com>
2021-05-18 22:56:09 +0530

    Update inline.py

--- tg_bot/modules/inline.py
+++ tg_bot/modules/inline.py
@@ -326,7 +326,7 @@ def character_query(query: str, update: Update, context: CallbackContext) -> Non
                           data=json.dumps({'query': CHAR_QUERY, 'variables': {'search': query}}),
                           headers={'Content-Type': 'application/json', 'Accept': 'application/json'})
         res = r.json()
-        data = res['data']['Page']['Character']
+        data = res.get.('data').get('Page').get('Character')
         res = data
         for data in res:
             ms_g = f"**{data.get('name').get('full') or ''}**(`{data.get('name').get('native') or ''}`)\n❤️ Favourites : {data['favourites'] or ''}\n"
