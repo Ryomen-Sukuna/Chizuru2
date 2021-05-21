0c0ed589cc9a3dafae27e50e11582d94f009c84e
Divu <70699947+imdivu@users.noreply.github.com>
2021-05-21 18:00:44 +0530

    Update dev.py

--- tg_bot/modules/dev.py
+++ tg_bot/modules/dev.py
@@ -1,16 +1,27 @@
-import os
-import subprocess
-import sys
-from time import sleep
-
-from tg_bot import dispatcher, telethn, OWNER_ID
-from tg_bot.modules.helper_funcs.chat_status import dev_plus
 from telegram import TelegramError, Update
 from telegram.ext import CallbackContext, CommandHandler
-import asyncio
-from statistics import mean
-from time import monotonic as time
-from telethon import events
+
+from tg_bot import dispatcher, telethn, OWNER_ID, RentalBot
+from tg_bot.modules.helper_funcs.chat_status import dev_plus
+
+
+@run_async
+@dev_plus
+def allow_groups(update: Update, context: CallbackContext):
+    args = context.args
+    if not args:
+        state = "Lockdown is " + "on" if not SaitamaRobot.ALLOW_CHATS else "off"
+        update.effective_message.reply_text(f"Current state: {state}")
+        return
+    if args[0].lower() in ["off", "no"]:
+        RentalBot.ALLOW_CHATS = True
+    elif args[0].lower() in ["yes", "on"]:
+        RentalBot.ALLOW_CHATS = False
+    else:
+        update.effective_message.reply_text("Format: /lockdown Yes/No or Off/On")
+        return
+    update.effective_message.reply_text("Done! Lockdown value toggled.")
+
 
 @dev_plus
 def leave(update: Update, context: CallbackContext):
@@ -27,84 +38,12 @@ def leave(update: Update, context: CallbackContext):
         update.effective_message.reply_text("Send a valid chat ID")
 
 
-@dev_plus
-def gitpull(update: Update, context: CallbackContext):
-    sent_msg = update.effective_message.reply_text(
-        "Pulling all changes from remote and then attempting to restart."
-    )
-    subprocess.Popen("git pull", stdout=subprocess.PIPE, shell=True)
-
-    sent_msg_text = sent_msg.text + "\n\nChanges pulled...I guess.. Restarting in "
-
-    for i in reversed(range(5)):
-        sent_msg.edit_text(sent_msg_text + str(i + 1))
-        sleep(1)
-
-    sent_msg.edit_text("Restarted.")
-
-    os.system("restart.bat")
-    os.execv("start.bat", sys.argv)
-
-@dev_plus
-def restart(update: Update, context: CallbackContext):
-    update.effective_message.reply_text(
-        "Starting a new instance and shutting down this one"
-    )
-
-    os.system("restart.bat")
-    os.execv("start.bat", sys.argv)
-
-class Store:
-    def __init__(self, func):
-        self.func = func
-        self.calls = []
-        self.time = time()
-        self.lock = asyncio.Lock()
-
-    def average(self):
-        return round(mean(self.calls), 2) if self.calls else 0
-
-    def __repr__(self):
-        return f"<Store func={self.func.__name__}, average={self.average()}>"
-
-    async def __call__(self, event):
-        async with self.lock:
-            if not self.calls:
-                self.calls = [0]
-            if time() - self.time > 1:
-                self.time = time()
-                self.calls.append(1)
-            else:
-                self.calls[-1] += 1
-        await self.func(event)
-
-async def nothing(event):
-    pass
-
-
-messages = Store(nothing)
-inline_queries = Store(nothing)
-callback_queries = Store(nothing)
-
-telethn.add_event_handler(messages, events.NewMessage())
-telethn.add_event_handler(inline_queries, events.InlineQuery())
-telethn.add_event_handler(callback_queries, events.CallbackQuery())
-
-
-@telethn.on(events.NewMessage(pattern=r"/getstats", from_users=OWNER_ID))
-async def getstats(event):
-    await event.reply(
-        f"**__KIGYO EVENT STATISTICS__**\n**Average messages:** {messages.average()}/s\n**Average Callback Queries:** {callback_queries.average()}/s\n**Average Inline Queries:** {inline_queries.average()}/s", parse_mode='md'
-        )
-
 
 LEAVE_HANDLER = CommandHandler("leave", leave, run_async=True)
-GITPULL_HANDLER = CommandHandler("gitpull", gitpull, run_async=True)
-RESTART_HANDLER = CommandHandler("reboot", restart, run_async=True)
+ALLOWGROUPS_HANDLER = CommandHandler("lockdown", allow_groups)
 
 dispatcher.add_handler(LEAVE_HANDLER)
-dispatcher.add_handler(GITPULL_HANDLER)
-dispatcher.add_handler(RESTART_HANDLER)
+dispatcher.add_handler(ALLOWGROUPS_HANDLER)
 
 __mod_name__ = "Dev"
-__handlers__ = [LEAVE_HANDLER, GITPULL_HANDLER, RESTART_HANDLER]
+__handlers__ = [LEAVE_HANDLER, ALLOWGROUPS_HANDLER]
