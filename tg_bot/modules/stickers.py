f5e88a733d85b3726d3019dfa7af4ad4b8324e24
Divu <70699947+imdivu@users.noreply.github.com>
2021-05-22 10:19:54 +0530

    Update stickers.py

--- tg_bot/modules/stickers.py
+++ tg_bot/modules/stickers.py
@@ -30,8 +30,8 @@ def cb_sticker(update: Update, context: CallbackContext):
     print("----------------------------------------")
     soup = BeautifulSoup(text, "lxml", from_encoding="utf-8")
     print(soup)
-    results = soup.find_all("a", {'class': "sticker-pack__btn"})
-    titles = soup.find_all("div", "sticker-pack__title")
+    results = soup.findAll("a", {'class': "sticker-pack__btn"})
+    titles = soup.findAll("div", "sticker-pack__title")
     if not results:
         msg.reply_text("No Results Found! :(")
         return
