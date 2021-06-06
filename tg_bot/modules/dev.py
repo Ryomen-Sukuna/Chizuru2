7e6e6c796849dd6f7564f2215e574be39abf8c3b
Divu <70699947+imdivu@users.noreply.github.com>
2021-06-06 18:16:35 +0530

    Update dev.py

--- tg_bot/modules/dev.py
+++ tg_bot/modules/dev.py
@@ -1,3 +1,5 @@
+from contextlib import suppress
+from telegram.error import Unauthorized
 from telegram import TelegramError, Update
 from telegram.ext import CallbackContext, CommandHandler
 
@@ -6,43 +8,29 @@ from tg_bot.modules.helper_funcs.chat_status import dev_plus
 
 
 @dev_plus
-def allow_groups(update: Update, context: CallbackContext):
-    args = context.args
-    if not args:
-        state = "Lockdown is " + "on" if not RentalBot.ALLOW_CHATS else "off"
-        update.effective_message.reply_text(f"Current state: {state}")
-        return
-    if args[0].lower() in ["off", "no"]:
-        RentalBot.ALLOW_CHATS = True
-    elif args[0].lower() in ["yes", "on"]:
-        RentalBot.ALLOW_CHATS = False
-    else:
-        update.effective_message.reply_text("Format: /lockdown Yes/No or Off/On")
-        return
-    update.effective_message.reply_text("Done! Lockdown value toggled.")
-
-
-@dev_plus
 def leave(update: Update, context: CallbackContext):
-    bot = context.bot
-    args = context.args
-    if args:
+    bot, args = context.bot, context.args
+
+    if args and len(args) > 5:
         chat_id = str(args[0])
         try:
             bot.leave_chat(int(chat_id))
-            update.effective_message.reply_text("Left chat.")
-        except TelegramError:
-            update.effective_message.reply_text("Failed to leave chat for some reason.")
+        except TelegramError as te:
+            if update.effective_chat.id != chat_id:
+                with suppress(TelegramError):
+                    update.effective_message.reply_text(f"I Could Not Leave That Group(Dunno Why Thooo). \n\nReason - TelegramError\n {te}")
+                return
+        if update.effective_chat.id != chat_id:
+            with suppress(Unauthorized):
+                update.effective_message.reply_text(f"I Could Not Leave That Group(Dunno Why Thooo).")
     else:
-        update.effective_message.reply_text("Send a valid chat ID")
+        update.effective_message.reply_text("Send a valid Chat Id.")
 
 
 
 LEAVE_HANDLER = CommandHandler("leave", leave, run_async=True)
-ALLOWGROUPS_HANDLER = CommandHandler("lockdown", allow_groups, run_async=True)
 
 dispatcher.add_handler(LEAVE_HANDLER)
-dispatcher.add_handler(ALLOWGROUPS_HANDLER)
 
 __mod_name__ = "Dev"
-__handlers__ = [LEAVE_HANDLER, ALLOWGROUPS_HANDLER]
+__handlers__ = [LEAVE_HANDLER]
