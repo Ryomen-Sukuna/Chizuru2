a6cdfb5c4463543a7a9745ea79c15e84641e9007
Divu <70699947+imdivu@users.noreply.github.com>
2021-06-14 11:51:48 +0530

    Update wallpaper.py

--- tg_bot/modules/wallpaper.py
+++ tg_bot/modules/wallpaper.py
@@ -6,7 +6,6 @@ from telegram import Update
 from telegram.ext import CallbackContext
 from tg_bot.modules.helper_funcs.decorators import kigcmd
 
-# Wallpapers module by @TheRealPhoenix using wall.alphacoders.com
 
 @kigcmd(command='wall')
 def wall(update: Update, context: CallbackContext):
@@ -37,18 +36,18 @@ def wall(update: Update, context: CallbackContext):
                 wallpaper = wallpapers[index]
                 wallpaper = wallpaper.get("url_image")
                 wallpaper = wallpaper.replace("\\", "")
-                bot.send_photo(
-                    chat_id,
-                    photo=wallpaper,
-                    caption="Preview",
-                    reply_to_message_id=msg_id,
-                    timeout=60,
-                )
                 bot.send_document(
                     chat_id,
                     document=wallpaper,
                     filename="wallpaper",
                     caption=caption,
+                    reply_to_message_id=msg_id,
+                    timeout=60,
+                )
+                bot.send_photo(
+                    chat_id,
+                    photo=wallpaper,
+                    caption="Preview",
                     reply_to_message_id=msg_id,
                     timeout=60,
                 )
