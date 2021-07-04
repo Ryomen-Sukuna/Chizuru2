40103acefbd4dda37e8c5a423fba1a9c53ed8ab4
Divu <70699947+imDivu@users.noreply.github.com>
2021-07-04 14:34:31 +0530

    Update purge.py

--- tg_bot/modules/purge.py
+++ tg_bot/modules/purge.py
@@ -38,10 +38,12 @@ async def purge_to(event):
     if event.fwd_from or event.from_id is None:
         return
 
-    if not await is_user_admin(
-            user_id=event.sender_id, message=event) and event.from_id not in [
-                1087968824
-            ]:
+    if (not await is_user_admin(
+                user_id=event.sender_id,
+                message=event,
+            )
+            and event.from_id not in [1087968824]
+       ):
         await event.reply("Only Admins are allowed to use this command")
         return
 
