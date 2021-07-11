8634a34544ff2fe7ff0abdca7f786aa6acfb61bf
Divu <70699947+imDivu@users.noreply.github.com>
2021-07-11 14:37:21 +0530

    Update afk.py

--- tg_bot/modules/afk.py
+++ tg_bot/modules/afk.py
@@ -134,12 +134,12 @@ def check_afk(update: Update, context: CallbackContext, user_id: int, fst_name: 
         since_afk = humanize.naturaldelta(datetime.now() - user.time)
         textmsg = f"{fname} Is AFK Since {since_afk}!"
 
-       print(user.messageid)
-       # try:
-       # if user.messageid != (None, ""):
-       #     context.bot.delete_message(int(user.messageid.split(' ', 1)[0]), int(user.messageid.split(' ', 1)[1]))
-       # except:
-       #     pass
+        print(user.messageid)
+        # try:
+        # if user.messageid != (None, ""):
+        #     context.bot.delete_message(int(user.messageid.split(' ', 1)[0]), int(user.messageid.split(' ', 1)[1]))
+        # except:
+        #     pass
         try:
             DND = update.effective_message.reply_text(
                       textmsg,
