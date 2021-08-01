fd8129b51308a29cd10c7de074ec0abc2e50adbd
Divu <70699947+imDivu@users.noreply.github.com>
2021-08-01 19:59:02 +0530

    Added /install

--- tg_bot/modules/dev.py
+++ tg_bot/modules/dev.py
@@ -1,6 +1,6 @@
 from contextlib import suppress
 from telegram.error import Unauthorized
-from telegram import TelegramError, Update
+from telegram import Update, ParseMode, TelegramError
 from telegram.ext import CallbackContext, CommandHandler
 
 from tg_bot import dispatcher
@@ -27,9 +27,36 @@ def leave(update: Update, context: CallbackContext):
         update.effective_message.reply_text("Send a valid Chat Id.")
 
 
+@dev_plus
+def pip_install(update: Update, context: CallbackContext):
+    message = update.effective_message
+    args = context.args
+    if not args:
+        message.reply_text("Enter a package name.")
+        return
+    if len(args) >= 1:
+        cmd = "py -m pip install {}".format(' '.join(args))
+        process = subprocess.Popen(
+            cmd.split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True,
+        )
+        stdout, stderr = process.communicate()
+        reply = ""
+        stderr = stderr.decode()
+        stdout = stdout.decode()
+        if stdout:
+            reply += f"*Stdout*\n`{stdout}`\n"
+        if stderr:
+            reply += f"*Stderr*\n`{stderr}`\n"
+
+        message.reply_text(text=reply, parse_mode=ParseMode.MARKDOWN)
+
+
 
 LEAVE_HANDLER = CommandHandler("leave", leave, run_async=True)
+PIP_INSTALL_HANDLER = CommandHandler("install", pip_install, run_async=True)
 dispatcher.add_handler(LEAVE_HANDLER)
+dispatcher.add_handler(PIP_INSTALL_HANDLER)
+
 
 __mod_name__ = "Dev"
-__handlers__ = [LEAVE_HANDLER]
+__handlers__ = [LEAVE_HANDLER, PIP_INSTALL_HANDLER]
