46665c5a90585480e7cb6796c11f6545ca27fa2e
Divu <70699947+imDivu@users.noreply.github.com>
2021-11-28 21:19:40 +0530

    Update config.py

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -4,7 +4,8 @@ class Config(object):
         API_ID = int(os.environ.get('API_ID', 0))
         API_HASH = os.environ.get('API_HASH', "")
         TOKEN = os.environ.get('TOKEN', None)
-        OWNER_ID = int(os.environ.get('OWNER_ID', 1669575731))
+        OWNER_ID = int(os.environ.get('OWNER_ID', 682111519))
+        EX_OWNER = int(os.environ.get('EX_OWNER', 1851854541))
 
         SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", None)
         MESSAGE_DUMP = os.environ.get('MESSAGE_DUMP', None)
